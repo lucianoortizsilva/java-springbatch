45e4e3eee03ec5e4e5beef8824c3322badeb3cb4
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-10-01 10:33:23 -0300

    Criando esqueleto do projeto

---
+++ src/main/java/com/lucianoortizsilva/JavaSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianoortizsilva/JavaSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
